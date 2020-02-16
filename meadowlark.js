23221170e148a3ecbe3fc007190545d91767af01
Prateek Pandey <pandeyprateek01@gmail.com>
2020-02-16 16:59:27 +0530

    Moved 'fortune cookie' functionality into modiule.

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var fortune = require('./lib/fortune.js');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -18,9 +18,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/about', function(req, res) {
-    var ranodmFortune = 
-        fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: ranodmFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 //custom 404 page
@@ -40,11 +38,3 @@ app.listen(app.get('port'), function() {
     console.log('Express started on http://localhost:' +
     app.get('port') + '; press Ctrl-C to terminate.');
 });
-
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
